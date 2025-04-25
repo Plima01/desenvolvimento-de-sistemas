6853e9f7115aa077a82d88983e69b3886af8da0d
Plima01 <pedrohenrique061377@gamail.com>
2025-04-24 22:02:06 -0300

    Dados, tipos de dados

---
+++ inicio/dados_02.js
@@ -0,0 +1,13 @@
+// tipos de dados
+
+let idade = 16;
+console.log(typeof 16);
+console.log(typeof idade);
+
+let salario = 1412.5;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo)
+const nome = "Pedro"
+console.log(typeof nome)
---
+++ inicio/desafio_dados.js
@@ -0,0 +1,13 @@
+/* DESAFIO: Crie um programa  que leia as variaveis PI e raio
+ para que seja possivel calcular o valor da área de circuferência (PI * raio²)
+ o programa deve imprimir na tela o valor calculado utilizando a concaterização de string
+ e varialve  */
+
+var PI = 3.14; 
+var RAIO = 10
+
+var area = PI *(RAIO * RAIO);
+
+var area = Match.PI * RAIO * RAIO;
+
+console.log( "O valor da área é: " + area + "m²");
